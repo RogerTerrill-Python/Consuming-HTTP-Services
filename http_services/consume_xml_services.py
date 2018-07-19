a9ff9e7c8b8430f3e086facf909d6a708bf72d17
Roger Dodger <lpaddikt@gmail.com>
2018-07-19 14:00:56 -0700

    XML parsing a live site with also using a counter, enumerate

--- http_services/consume_xml_services.py
+++ http_services/consume_xml_services.py
@@ -2,18 +2,30 @@ import requests
 from xml.etree import ElementTree
 import collections
 from dateutil.parser import parse
-# 3:46
 
 Episode = collections.namedtuple('Episode', 'title link pubdate')
+
 
 def main():
     dom = get_xml_dom('https://talkpython.fm/rss')
     episodes = get_episodes(dom)
+    for idx, e in enumerate(episodes[:5]):
+        print(f'{idx}. {e.title}')
 
 
 def get_episodes(dom):
     item_nodes = dom.findall('channel/item')
-    print(len(item_nodes))
+
+    episodes = [
+        Episode(
+            n.find('title').text,
+            n.find('link').text,
+            parse(n.find('pubDate').text)
+        )
+        for n in item_nodes
+    ]
+
+    return sorted(episodes, key=lambda e: e.pubdate)
 
 
 def get_xml_dom(url):
